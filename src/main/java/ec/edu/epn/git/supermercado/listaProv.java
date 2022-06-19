16f38db40a06ee8335a194c2fbfe1174dc3d0347
Fernando Caranqui <fernandocaranqui03@gmail.com>
2022-06-19 14:33:00 -0500

    Se ha creado insertado la funcionalidad Agregar y consultar Proveedor

---
+++ src/main/java/ec/edu/epn/git/supermercado/listaProv.java
@@ -0,0 +1,35 @@
+package ec.edu.epn.git.supermercado;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class listaProv {
+    ArrayList<proveedor> listaProvedores = new ArrayList<proveedor>();
+    //Llenar datos del proveedor
+    proveedor proveedor;
+
+    public proveedor provedorDatos() throws IOException {
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Nombre:");
+        String name = teclado.readLine();
+        System.out.println("Empresa:");
+        String company = teclado.readLine();
+        System.out.println("Dias de Visita:");
+        String days = teclado.readLine();
+        proveedor = new proveedor(name, company,days);
+
+        return proveedor;
+    }
+    public void addProv(proveedor proveedor){
+        listaProvedores.add(proveedor);
+    }
+    //Muestra la lista de Proveedores
+    public void mostrarLista(){
+        for(int i = 0; i < listaProvedores.size() ; i++){
+            System.out.println(listaProvedores.get(i));
+        }
+    }
+}
---
+++ src/test/java/ec/edu/epn/git/supermercado/ProveedorTest.java
@@ -0,0 +1,19 @@
+package ec.edu.epn.git.supermercado;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ProveedorTest {
+     @Test
+    public void addprovecdorTest() throws IOException {
+        listaProv lista = new listaProv();
+        String name = "Kevin Caranqui";
+        String company = "Coca-Cola";
+        String days = "Lunes-Jueves";
+        proveedor prove = new proveedor(name, company,days);
+        lista.addProv(prove);
+        lista.mostrarLista();
+    }
+}
