011cd1c5a81caa0be3221913e7908e04f1cd4134
Fernando Caranqui <fernandocaranqui03@gmail.com>
2022-06-21 21:56:37 -0500

    Se agregó metodo de autentificacion de usuario a la clase ListaPedido

--- src/main/java/ec/edu/epn/git/supermercado/ListaPedido.java
+++ src/main/java/ec/edu/epn/git/supermercado/ListaPedido.java
@@ -5,10 +5,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
 
 public class ListaPedido {
     ArrayList<Pedido> listaPedido = new ArrayList<Pedido>();
     Pedido pedido;
+    login newlogin;
 
     public Pedido datosPedido() throws IOException { //se llenan los datos del pedido
         BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
@@ -35,17 +37,39 @@ public class ListaPedido {
             System.out.println(listaPedido.get(i));
         }
     }
+    //opcion guardar
+    public boolean guardarArchivo(){
+        System.out.println("|---------¿Desea exportar factura?---------|");
+        System.out.println("|-----[Si]-digite-1----[No]-digite-0--------|");
+        Scanner sc = new Scanner(System.in);
+        int respuesta = sc.nextInt();
+        return (respuesta == 1);
+    }
+    //iniciar sesion
+    public boolean singin(){
+        newlogin = new login("admin", "123");
+        newlogin.aunticar();
+        return true;
+    }
+
     //exportar Archivo
-    public void export(){
-        String DetallePedido = "Factura"+ pedido.getNameProd() + pedido.getFechaPedido();
-        try{
-            FileWriter archivo = new FileWriter("C:\\Users\\yagua\\Desktop\\DetallePedido+ pedido.getNameProd() + pedido.getFechaPedido().txt", true);
-            try(BufferedWriter almacen = new BufferedWriter(archivo)){
-                almacen.write(listaPedido + "\n");
-                almacen.close();
+    public void export() {
+        if(guardarArchivo()){
+            if(singin()){
+                String DetallePedido = "Factura"+ pedido.getNameProd() + pedido.getFechaPedido();
+                try{
+                    FileWriter archivo = new FileWriter("C:\\Users\\yagua\\Desktop\\DetallePedido.txt", true);
+                    try(BufferedWriter almacen = new BufferedWriter(archivo)){
+                        almacen.write(listaPedido + "\n");
+                        almacen.close();
+                    }
+                    archivo.close();
+                    System.out.println("Se ha guardado y exportado la factura con éxito");
+                }catch (Exception ex){}
             }
-            archivo.close();
-        }catch (Exception ex){}
+        }else{
+            System.out.println("GRACIAS VUELVA PRONTO");
+        }
     }
 
 }
