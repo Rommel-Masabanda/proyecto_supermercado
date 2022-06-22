06c9ecf71358f9a9739ee820d6a231c95ec87f28
Fernando Caranqui <fernandocaranqui03@gmail.com>
2022-06-21 21:58:36 -0500

    Se agregó pruebas en la calse main

--- src/main/java/ec/edu/epn/git/supermercado/Main.java
+++ src/main/java/ec/edu/epn/git/supermercado/Main.java
@@ -4,28 +4,36 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String args[]) throws IOException {
-        /*
+
         ModuloProducto moduloProducto = new ModuloProducto();
         moduloProducto.iniciarArchivoProducto();
         moduloProducto.agregarNuevoProducto();
         moduloProducto.mostrarProductosEnPantalla();
-        */
+
 
         ModuloFacturaVenta mfv = new ModuloFacturaVenta();
         mfv.iniciarArchivoFacturaVenta();
         mfv.agregarNuevoFacturaVenta();
         mfv.mostrarFacturaVentaEnPantalla();
 
+
+
         /*listaProv lista = new listaProv();
         lista.addProv(lista.provedorDatos());
         lista.mostrarLista();*/
+
+    /*
+        ListaPedido nuevaLista = new ListaPedido();
+        nuevaLista.addPedido(nuevaLista.datosPedido());
+        nuevaLista.showListaPedidos();
+        nuevaLista.export();
 
 
         ListaPedido nuevaLista = new ListaPedido();
         nuevaLista.addPedido(nuevaLista.datosPedido());
         nuevaLista.showListaPedidos();
         nuevaLista.export();
-
+*/
     }
 
 }
